7f33ba987f135fb26a8839012cdb8f2096b2b3ce
basicactor <dreamengine316@gmail.com>
2018-03-16 00:55:20 -0400

    sendgrid3

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
- :address        => 'smtp.sendgrid.net',
- :port           => '587',
- :authentication => :plain,
- :user_name      => ENV['paperhelp316'],
- :password       => ENV['Paper316316Help'],
- :domain         => 'paperhelp.herokuapp.com',
- :enable_starttls_auto => true
-}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,20 +34,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    #:address => "smtp.gmail.com",
-    #:port => 587,
-    #:user_name => "paperhelp316@gmail.com",
-    #:password => "PaperHelp357",
-    #:authentication => :plain,
-    #:enable_starttls_auto => true
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "paperhelp316@gmail.com",
+    :password => "PaperHelp357",
+    :authentication => :plain,
+    :enable_starttls_auto => true
 
-     :user_name      => ENV['SENDGRID_USERNAME'],
-     :password       => ENV['SENDGRID_PASSWORD'], 
-     :address        => 'smtp.sendgrid.net',
-     :port           => '587',
-     :authentication => :plain,    
-     :domain         => 'localhost:3000',
-     :enable_starttls_auto => true
+    
   }
 
   config.action_mailer.perform_caching = false
