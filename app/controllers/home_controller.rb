007918ce4733db5b79e28a60b63627e2fe4a5e86
basicactor <dreamengine316@gmail.com>
2018-03-15 22:42:39 -0400

    authorised to enter toppage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user, only[:main]
 	before_action :correct_user, only: [:main]
+  
   def top
   end
 
