4d50ea806f4509983110a1b05f83487d87bbe64a
basicactor <dreamengine316@gmail.com>
2018-03-15 22:01:39 -0400

    changed mailer setting in production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['basicactor'],
+  :password       => ENV['Paper316316Help'],
+  :domain         => 'paperhelp.herokuapp.com',
+  :enable_starttls_auto => true
+}
