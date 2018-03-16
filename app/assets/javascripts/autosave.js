765da4391b18c6ae7a182da2ba3ecf3050ee83ee
basicactor <dreamengine316@gmail.com>
2018-03-16 16:24:38 -0400

    fixed autosave function

--- app/assets/javascripts/autosave.js
+++ app/assets/javascripts/autosave.js
@@ -9,15 +9,28 @@ $(document).on('turbolinks:load', function(){
             $(".done").text('saving...');
             window.tempTimer = window.setTimeout(function(){
                 $(".done").text('All changes saved');
-                $(".edit_section").ajaxSubmit({
-                	data:{"authenticity_token": $('meta[name="csrf-token"]').attr('content')}
+                updateText();　//定義した関数を使う
 
-                });
-
-            }, 4000);
+            }, 3500);
             }));
         
    }
+
+//ajaxの関数を定義
+function updateText(){ 
+    var sectionTitle = $('#section_title').val();
+    var sectionText = $('#section_text').val();
+
+    $.ajax({ 
+      url:$(this).attr('action'), 
+      type: 'PATCH', 
+      data:{"section": {"title":sectionTitle, "text":sectionText},
+          "authenticity_token" : $('meta[name="csrf-token"]').attr('content')
+        }
+
+    })
+
+}
 
 
 
