e645b90e7c1478227c8800702e7cbeb116fc4241
basicactor <dreamengine316@gmail.com>
2018-03-16 01:49:03 -0400

    unabled timeoutable from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
+         :confirmable, :lockable, :omniauthable, omniauth_providers: [:twitter]
 
   has_many :papers, dependent: :destroy
 
