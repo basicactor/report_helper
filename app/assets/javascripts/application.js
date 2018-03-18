dbd294ff1b7c825ff0030c6aa8988964e4034412
basicactor <dreamengine316@gmail.com>
2018-03-18 16:01:27 -0400

    fixed modal show problem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,73 @@
 //= require jquery-ui
 //= require turbolinks
 //= require_tree .
+
+
+ready = function() {
+  $(document).on('turbolinks:load', function(){
+
+    // outline-areaでpaper-title変更
+    $('#edit-paper-title-modal-trigger').on('click',function(){
+      $('#edit-paper-title-modal-show').fadeIn();
+    });
+
+    // Dashboardでpaper-title変更 
+     $('.paper-edit').on('click',function(){
+      $(this).closest('.paper-item').find('.paper-title-edit-in-dashboard-show').addClass('modal-active');
+    });
+
+    // Dashboardで新規ペーパー作成
+     $('#paper-create-modal-trigger').on('click',function(){
+        $('#new-paper-modal-create-show').fadeIn();
+     });
+
+     // Headerでprofileボタンクリック -> モーダル
+    $('#profile-modal-trigger').on('click', function(){
+      $('#profile-modal-show').fadeIn();
+    });
+
+     // Citation-areaでCitation編集ボタンクリック -> モーダル
+    $('.citation-edit').on('click',function(){
+      $(this).closest('.citation-item').find('.edit-citation-modal-show').addClass("modal-active");
+    });
+
+
+
+
+  $(document).click(function(e) {
+  // クリックした場所がmenu-wrapper(領域内とみなす範囲)に無ければmenuを消す
+  //containsはDOM要素しか引数として取らない
+  //$('something')[0]でjQueryオブジェクトをDOM要素に変換
+
+    if(!$.contains($('.edit-paper-title-in-outline')[0] || $('.paper-list')[0], e.target)){
+      // outline-areaでpaper-title変更モーダルを隠す
+      $('#edit-paper-title-modal-show').hide();
+    
+      // Dashboardでpaper-title変更モーダルを隠す
+      $('.paper-title-edit-in-dashboard-show').removeClass('modal-active');
+
+      // Dashboardで新規ペーパー作成モーダルを隠す
+      $('#new-paper-modal-create-show').hide();
+    }
+    
+    // Citaion-areaでCitation変更モーダルを隠す 
+    if(!$.contains($('#citation-list')[0], e.target)){
+      $('.edit-citation-modal-show').removeClass('modal-active');
+    }
+
+    //headerのprofile隠す
+    if(!$.contains($('.header-menu')[0], e.target)){
+      $('#profile-modal-show').hide();
+    }
+
+  });
+
+
+
+
+
+});
+};
+$(document).ready(ready)
+$(document).on('page:load', ready)
+
