831d2e0651e21f1cd68b3877601477c011c98cb6
basicactor <dreamengine316@gmail.com>
2018-10-29 23:29:45 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   get '/dashboard' => "papers#dashboard"
 
+  get '/demo' => "sections#demo"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
